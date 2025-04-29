6afc86a72fdf7ae12617a1e9047b9a4d5aac765b
carter-oleary <carter.m.oleary@outlook.com>
2025-04-29 15:47:07 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Write("Customer name: ");
+string name = Console.ReadLine();
+Console.Write("Units Consumed: ");
+double units = Convert.ToDouble(Console.ReadLine());
+
+double total = CalculateBill(units);
+Console.WriteLine($"{name}'s bill total is {total.ToString("C2")}");
+
+double CalculateBill(double d)
+{
+    switch(d)
+    {
+        case < 200: return d * 1.2;
+        case < 400: return 238.8 + (d - 199) * 1.5;
+        case < 600: return 238.8 + 300 + (d - 399) * 1.8;
+        default: return 238.8 + 300 + 360 + (d - 599) * 2;
+    }
+}
